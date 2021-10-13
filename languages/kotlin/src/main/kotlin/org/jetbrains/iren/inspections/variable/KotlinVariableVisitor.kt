2d0bc9c8535310360a1f656862808bd52975a12d
Igor.Davidenko <igor.davidenko@jetbrains.com>
2021-10-13 18:41:27 +0300

    fix bug (inspection visited not all places)

--- languages/kotlin/src/main/kotlin/org/jetbrains/iren/inspections/variable/KotlinVariableVisitor.kt
+++ languages/kotlin/src/main/kotlin/org/jetbrains/iren/inspections/variable/KotlinVariableVisitor.kt
@@ -2,6 +2,7 @@ package org.jetbrains.iren.inspections.variable
 
 import com.intellij.codeInspection.ProblemsHolder
 import org.jetbrains.iren.utils.RenameUtils
+import org.jetbrains.kotlin.psi.KtParameter
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.KtVisitorVoid
 
@@ -11,6 +12,14 @@ class KotlinVariableVisitor(private val holder: ProblemsHolder) : KtVisitorVoid(
             RenameUtils.visitVariable(property, holder)
         } finally {
             super.visitProperty(property)
+        }
+    }
+
+    override fun visitParameter(parameter: KtParameter) {
+        try {
+            RenameUtils.visitVariable(parameter, holder)
+        } finally {
+            super.visitParameter(parameter)
         }
     }
 }
--- languages/python/src/main/kotlin/org/jetbrains/iren/inspections/variable/PyVariableVisitor.kt
+++ languages/python/src/main/kotlin/org/jetbrains/iren/inspections/variable/PyVariableVisitor.kt
@@ -2,6 +2,7 @@ package org.jetbrains.iren.inspections.variable
 
 import com.intellij.codeInspection.ProblemsHolder
 import com.jetbrains.python.psi.PyElementVisitor
+import com.jetbrains.python.psi.PyNamedParameter
 import com.jetbrains.python.psi.PyTargetExpression
 import org.jetbrains.iren.utils.RenameUtils
 
@@ -11,6 +12,14 @@ class PyVariableVisitor(private val holder: ProblemsHolder) : PyElementVisitor()
             RenameUtils.visitVariable(node, holder)
         } finally {
             super.visitPyTargetExpression(node)
+        }
+    }
+
+    override fun visitPyNamedParameter(node: PyNamedParameter) {
+        try {
+            RenameUtils.visitVariable(node, holder)
+        } finally {
+            super.visitPyNamedParameter(node)
         }
     }
 }
