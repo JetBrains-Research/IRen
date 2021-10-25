f539ff3ad1254f6fb37d32c0232b02f468b9018f
Igor.Davidenko <igor.davidenko@jetbrains.com>
2021-10-25 13:03:53 +0300

    refactoring in NotificationsUtil

--- languages/common/src/main/java/org/jetbrains/iren/PluginLoadedListener.java
+++ languages/common/src/main/java/org/jetbrains/iren/PluginLoadedListener.java
@@ -2,19 +2,13 @@ package org.jetbrains.iren;
 
 import com.intellij.ide.plugins.DynamicPluginListener;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationAction;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.iren.services.ConsistencyChecker;
 import org.jetbrains.iren.services.ModelManager;
-import org.jetbrains.iren.settings.AppSettingsState;
 import org.jetbrains.iren.utils.LanguageSupporter;
+
+import static org.jetbrains.iren.utils.NotificationsUtil.askPermissions;
 
 public class PluginLoadedListener implements DynamicPluginListener {
     @Override
@@ -29,35 +23,5 @@ public class PluginLoadedListener implements DynamicPluginListener {
         DynamicPluginListener.super.pluginLoaded(pluginDescriptor);
         LanguageSupporter.removeRenameHandlers();
         askPermissions();
-    }
-
-    public static void askPermissions() {
-        AppSettingsState settings = AppSettingsState.getInstance();
-        if (settings.firstOpen) {
-            Notification notification = new Notification(IRenBundle.message("name"),
-                    "IRen: automatic training permission",
-                    "Do You allow automatic training of models?",
-                    NotificationType.INFORMATION);
-            notification.addAction(new NotificationAction("Yes (default)") {
-                @Override
-                public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
-                    settings.automaticTraining = true;
-                    settings.firstOpen = false;
-                    notification.expire();
-                    @Nullable Project project = e.getProject();
-                    if (project == null) return;
-                    ModelBuilder.trainInBackground(project);
-                }
-            });
-            notification.addAction(new NotificationAction("No") {
-                @Override
-                public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
-                    settings.automaticTraining = false;
-                    settings.firstOpen = false;
-                    notification.expire();
-                }
-            });
-            Notifications.Bus.notify(notification);
-        }
     }
 }
--- languages/common/src/main/java/org/jetbrains/iren/utils/NotificationsUtil.java
+++ languages/common/src/main/java/org/jetbrains/iren/utils/NotificationsUtil.java
@@ -1,11 +1,21 @@
 package org.jetbrains.iren.utils;
 
 import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationAction;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.registry.Registry;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.iren.IRenBundle;
+import org.jetbrains.iren.ModelBuilder;
+import org.jetbrains.iren.settings.AppSettingsState;
+
+import java.nio.file.Path;
+
+import static com.intellij.ide.actions.RevealFileAction.openDirectory;
 
 public class NotificationsUtil {
     /**
@@ -24,5 +34,47 @@ public class NotificationsUtil {
 
     public static boolean isVerboseInference() {
         return Registry.get("iren.verbose.inference").asBoolean();
+    }
+
+    public static void notificationAboutModel(@NotNull Project project, @NotNull String title, @NotNull String text, @Nullable Path modelPath) {
+        final Notification notification = new Notification(IRenBundle.message("name"),
+                title,
+                text,
+                NotificationType.INFORMATION);
+        if (modelPath != null) notification.addAction(new NotificationAction("Open model's directory") {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
+                openDirectory(modelPath);
+            }
+        });
+        Notifications.Bus.notify(notification, project);
+    }
+
+    public static void askPermissions() {
+        AppSettingsState settings = AppSettingsState.getInstance();
+        if (settings.firstOpen) {
+            Notification notification = new Notification(IRenBundle.message("name"),
+                    "IRen: automatic training permission",
+                    "Do You allow automatic training of models?",
+                    NotificationType.INFORMATION);
+            notification.addAction(new NotificationAction("Yes (default)") {
+                @Override
+                public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
+                    settings.automaticTraining = true;
+                    settings.firstOpen = false;
+                    notification.expire();
+                    ModelBuilder.trainModelsForAllProjectsInBackground();
+                }
+            });
+            notification.addAction(new NotificationAction("No") {
+                @Override
+                public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
+                    settings.automaticTraining = false;
+                    settings.firstOpen = false;
+                    notification.expire();
+                }
+            });
+            Notifications.Bus.notify(notification);
+        }
     }
 }
